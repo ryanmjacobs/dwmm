d0ada65cde9880d7a23289815402d481be49d614
Ryan Jacobs <ryan.mjacobs@gmail.com>
2015-04-09 23:38:22 -0700

    cleanup

--- all.js
+++ all.js
@@ -1,21 +1,30 @@
-var button = document.getElementById("#button");
-var video  = document.getElementById("#video");
+var video = document.getElementById("#video");
+video.pause();
 
 function fadeIn() {
     video.classList.remove("video-off");
-}
+};
 function fadeOut() {
     video.classList.add("video-off");
-}
-
-video.onclick = function() {
-    if (video.paused) {
-        video.play();
-        fadeIn();
-    } else {
-        fadeOut();
-        video.pause();
-    }
 };
 
-video.pause();
+var setClickHook = function() {
+    video.onclick = function() {
+        if (video.paused) {
+            video.play();
+            fadeIn();
+        } else {
+            fadeOut();
+            video.pause();
+        }
+    };
+};
+
+/* TODO: wait for some buffering to happen first -- show loading stuff
+setInterval(function() {
+    var p = video.buffered.end(0) / video.duration;
+    console.log(p);
+}, 300);
+*/
+
+setTimeout(setClickHook, 1000);
